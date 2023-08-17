f5735668fd8076c6db6472297f6991e081fa5e4c
Daniel25A <gomezoscar089@gmail.com>
2023-08-17 11:53:15 -0400

    Code

---
+++ PrinterApp/Services/PrintService.cs
@@ -0,0 +1,41 @@
+﻿using PrinterApp.Enums;
+using System.Drawing.Printing;
+//Codigo Escrito por Daniel25A ( Oscar Gomez)  Ingeniero de Software
+namespace PrinterApp.Services
+{
+    public class PrintService
+    {
+        public static PrintService getInstance= new ();
+        int pointerX = 0xF001;
+        int pointerY = 0xF002;
+        public Task<PrintResult> PrintRaw(
+            string content,int quantity)
+        {
+            try
+            {
+              using  PrintDocument printer = new();
+                printer.PrintPage += delegate (object sender, PrintPageEventArgs e)
+                {
+                    e?.Graphics?.DrawString(content, new Font("Times New Roman", 12), 
+                        new SolidBrush(Color.Black), new RectangleF(0, 0, printer.DefaultPageSettings.PrintableArea.Width,
+                        printer.DefaultPageSettings.PrintableArea.Height));
+
+                };
+                try
+                {
+                    printer.Print();
+                    return Task.FromResult(PrintResult.Success);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Ocurrio un Error !", ex);
+                }
+            }
+            catch (Exception)
+            {
+               return Task.FromResult(PrintResult.Error);
+            }
+        }
+
+    }
+}
